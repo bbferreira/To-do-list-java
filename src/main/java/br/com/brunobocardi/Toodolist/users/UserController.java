97812d9e979bcc3c130111edde29fad05fb8230d
bbferreirar <brunobocardiferreiraite@gmail.com>
2023-10-14 21:04:07 -0300

    Commit aula 02

--- src/main/java/br/com/brunobocardi/Toodolist/users/UserController.java
+++ src/main/java/br/com/brunobocardi/Toodolist/users/UserController.java
@@ -1,5 +1,8 @@
 package br.com.brunobocardi.Toodolist.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +21,24 @@ public class UserController {
      * Date (data)
      * void (vazio)
      */
+    @Autowired
+    private IUserRepository useRepository;
 
     @PostMapping("/")
-    public  void create(@RequestBody  UserModel userModel){
-        System.out.println(userModel.getUsername());
+    public  ResponseEntity   create(@RequestBody  UserModel userModel){
+       var user =  this.useRepository.findByUsername(userModel.getUsername());
+
+
+        if(user != null){
+        
+            //Mensagem de erro
+            //Status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já existe");
+        }
+
+
+         var  userCreated  =  this.useRepository.save(userModel);
+         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 
     }
     
--- src/main/java/br/com/brunobocardi/Toodolist/users/UserModel.java
+++ src/main/java/br/com/brunobocardi/Toodolist/users/UserModel.java
@@ -1,28 +1,53 @@
 package br.com.brunobocardi.Toodolist.users;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/**
+ * Importando o Lombook 
+ * Ao usar a anotação @Data do Lombok, ele automaticamente gera getters, setters, métodos equals, 
+ * hashCode, toString e muito mais, com base nos campos da classe. 
+ * Isso simplifica o desenvolvimento,
+ *  pois você não precisa escrever esses métodos manualmente.
+ */
+
+@Data   /*@Data define tanto os getters quanto os setters */
+@Entity(name="tb_users")
+
 public class UserModel {
 
 
+@Id
+@GeneratedValue(generator = "UUID")
+private  UUID  id;
 
-
+@Column(unique = true)
 private  String username;
     private String name;
     private  String password;
 
 
 /**
- * Usar o modificador private 
+ * Usar o modificador private :sem a biblioteca Lombook
  * Inserir o valor => set 
- */
-    public void setUsername(String username) {
+ *  public void setUsername(String username) {
     this.username = username;
     }
+ */
+   
 
     /**
      * 
      *  Buscar o valor => Get
-     */
-    public String getUsername() {
+     *     public String getUsername() {
     return username;
     }
 
@@ -42,8 +67,11 @@ private  String username;
         return password;
     }
 
+     */
 
 
+@CreationTimestamp
+private LocalDateTime createAt;
 
 
 
